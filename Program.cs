6086b1bdece14a27e21f95b55ddfb03ac2f425cb
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 17:01:30 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿// Pedir números hasta que el usuario ingrese 0. 
+int num = 1;
+
+while (num !=0)
+{
+    Console.WriteLine("Ingrese números");
+    num = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("El número ingresado es 0");
